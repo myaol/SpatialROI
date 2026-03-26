fd55555941989e0cdfe449ee397df8281046695a
Haoyue Dai <my.lu@pitt.edu>
2026-03-26 15:57:32 -0400

    add the DEG size limit

--- R/spatial_function.R
+++ R/spatial_function.R
@@ -1423,6 +1423,7 @@ run_spatial_selector <- function(seurat_input, sample_name = "sample", show_imag
 
     # Increase file upload size limit (default is 5MB, set to 500MB)
     options(shiny.maxRequestSize = 500*1024^2)  # 500MB in bytes
+    options(future.globals.maxSize = 8000 * 1024^2) 
     # ── Fix for Seurat FindMarkers future globals error ──
 
 
