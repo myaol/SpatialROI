b9989ae841aafdab122981662ee46651f0add821
myaol <my.lu@pitt.edu>
2026-05-30 17:40:49 +0800

    test files

--- tests/testthat/test-app_launch.R
+++
@@ -1,3 +0,0 @@
-test_that("SpatialScope app can launch", {
-  expect_true(is.function(SpatialScope::run_spatial_selector))
-})
--- tests/testthat/test-example-data.R
+++
@@ -1,4 +0,0 @@
-# tests/testthat/test-example-data.R
-test_that("Example data loads", {
-  expect_true(file.exists(system.file("app", package = "SpatialScopeDev")))
-})
